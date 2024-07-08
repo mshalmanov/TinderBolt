6a6c422170d265fb8f3c832013050588f5ea0214
Marat <1203Marat@gmail.com>
2024-07-09 00:30:15 +0600

    add telegram-bot to project

--- README.md
+++ README.md
@@ -1,21 +1,36 @@
 # TinderBolt
-TinderBolt is telegram bot with ChatGPT and combines the dating app tinder
+**TinderBolt** is telegram bot with ChatGPT and combines the dating app tinder
 
 # Install and Configure
 
-* clone project to your local workspace and moving to a project folder*
-git clone 
+- clone project to your local workspace and moving to a project folder    
+```bash
+git clone
+``` 
+
+```bash
 cd TinderBolt
+```
 
-* create venv in the project folder. *
-python -m venv venv
+- create venv in the project folder.
+```bash
+python -m venv .venv
+```
 
-* .venv activation for Windows. *
-venv\Scripts\activate.bat
+- .venv activation for Windows.
+```bash
+.venv\Scripts\activate.bat
+```
 
-* .venv activation for Linux and MacOS. *
-source venv/bin/activate
+- .venv activation for Linux and MacOS.
+```bash
+source .venv/bin/activate
+```
 
-* Install from pip telegram-bot and openai libraries *
+- Install from pip telegram-bot and openai libraries
+```bash
 pip install python-telegram-bot
+```
+```bash
 pip install openai
+```
--- bot.py
+++ bot.py
@@ -4,7 +4,32 @@ from gpt import *
 from util import *
 
 # тут будем писать наш код :)
+async def start(update, context):
+    text = load_message("main")
+    await send_photo(update, context, "main")
+    await send_text(update, context, text)
+
+async def hello(update, context):
+    await send_text(update, context, "*Привет*")
+    await send_text(update, context, "_Как дела?_")
+    await send_text(update, context, "Вы написали " + update.message.text)
+
+    await send_photo(update, context, "avatar_main")
+    await send_text_buttons(update, context, "Запустить процесс?", {
+        "start":"Запустить",
+        "stop":"Остановить"
+    })
+
+async def hello_button(update, context):
+    query = update.callback_query.data
+    if query == "start":
+        await send_text(update, context, "Процесс запущен")
+    else:
+        await send_text(update, context, "Процесс остановлен")
 
 
 app = ApplicationBuilder().token("telegram-token").build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
+app.add_handler(CallbackQueryHandler(hello_button))
 app.run_polling()
