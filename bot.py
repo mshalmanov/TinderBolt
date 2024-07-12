126c09ec622eaba9f1fc1dfa1f29d5da420f5987
Marat <1203Marat@gmail.com>
2024-07-12 22:59:31 +0600

    add date  function to project

--- bot.py
+++ bot.py
@@ -30,9 +30,39 @@ async def gpt_dialog(update, context):
     answer = await chatgpt.send_question(prompt,text)
     await send_text(update, context, answer)
 
+async def date(update, context):
+    dialog.mode = "date"
+    text = load_message("date")
+    await send_photo(update, context, "date")
+    await send_text_buttons(update, context, text, {
+        "date_grande": "Ариана Гранде",
+        "date_robbie": "Марго Робби",
+        "date_zendaya": "Зендея",
+        "date_gosling": "Райан Гослинг",
+        "date_hardy": "Том Харди"
+    })
+
+async def date_dialog(update, context):
+    text = update.message.text
+    my_message = await send_text(update, context, "Девушка (парень) набирает текст...")
+    answer = await chatgpt.add_message(text)
+    await my_message.edit_text(answer)
+
+async def date_button(update, context):
+    query = update.callback_query.data
+    await update.callback_query.answer()
+
+    await send_photo(update, context, query)
+    await send_text(update, context, "Отличный выбор! Пригласите девушку (парня) на свидание за 5 сообщений")
+
+    prompt = load_prompt(query)
+    chatgpt.set_prompt(prompt)
+
 async def hello(update, context):
     if dialog.mode == "gpt":
         await gpt_dialog(update, context)
+    elif dialog.mode == "date":
+        await date_dialog(update, context)
     else:
         await send_text(update, context, "*Привет*")
         await send_text(update, context, "_Как дела?_")
@@ -59,6 +89,10 @@ chatgpt = ChatGptService(token="chatgpt-token")
 app = ApplicationBuilder().token("telegram-token").build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("gpt", gpt))
+app.add_handler(CommandHandler("date", date))
+
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
+
+app.add_handler(CallbackQueryHandler(date_button, pattern="^date_.*"))
 app.add_handler(CallbackQueryHandler(hello_button))
 app.run_polling()
